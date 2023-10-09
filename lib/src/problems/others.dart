3f5f801fc95522f9fdbba4bad4067b8c39edccc9
Stephen Obotu Oduh <osobotu@gmail.com>
2023-10-10 00:14:32 +0100

    added print nested list and other recursion techniques.

--- lib/src/problems/others.dart
+++ lib/src/problems/others.dart
@@ -1,25 +1,3 @@
-/// Write a recursion function that takes an array of integers and other
-/// arrays which in turn hold integers. Your task is the print all the
-/// numbers and just the numbers.
-///
-/// So i modified it to return a flattened list of integers so I test
-
-List<int> printAllNumbers(List<dynamic> nestedArray) {
-  List<int> result = [];
-  for (int i = 0, n = nestedArray.length; i < n; i++) {
-    if (nestedArray[i] is int) {
-      /// check if it prints at all
-      // print(nestedArray[i]);
-
-      result.add(nestedArray[i]);
-    } else {
-      final r = printAllNumbers(nestedArray[i]);
-      result.addAll(r);
-    }
-  }
-  return result;
-}
-
 int getXIndex(String text, [int index = 0]) {
   text = text.toLowerCase();
   if (text[index] == 'x') return index;
